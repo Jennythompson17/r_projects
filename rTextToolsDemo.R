e06b4485ff91f12c15fad063c872be7ec56e6b87
JennyThompson17 <jennythompson17@gmail.com>
2014-12-12 12:55:47 +0000

    add containers, train/test set partition, SVM, GLMNET, MAXENT

--- rTextToolsDemo.R
+++ rTextToolsDemo.R
@@ -5,4 +5,16 @@ install.packages("RTextTools")
 install.packages("tm")
 
 ##Load USCongress dataset - comes with RTextTools##
+data(USCongress)
 
+## CREATE THE DOCUMENT-TERM MATRIX##
+doc_matrix <- create_matrix(USCongress$text, language="english", removeNumbers=TRUE,
+                            stemWords=TRUE, removeSparseTerms=.998)
+
+## Create the container and divide test set from training set##
+container <- create_container(doc_matrix, USCongress$major, trainSize=1:4000,testSize=4001:4449, virgin=FALSE)
+
+## Apply Support Vector Machine, glmnet and maximum entropy models ##
+SVM <- train_model(container,"SVM")
+GLMNET <- train_model(container,"GLMNET")
+MAXENT <- train_model(container,"MAXENT")
